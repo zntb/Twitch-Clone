9f1fbfa5d9b2231015773401c22c7f5576773d6d
zntb <zenetibi@yahoo.com>
2023-11-21 15:53:36 +0200

    Add Logo component

--- client/src/AuthPage/Login.js
+++ client/src/AuthPage/Login.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import { Logo } from './Logo';
 
 export const Login = ({ switchAuthHandler }) => {
   return (
     <div className="login-container">
-      <p>Logo</p>
+      <Logo text={'Log in to Clone'} />
       <form className="auth-form">Form</form>
       <span onClick={switchAuthHandler} className="auth-form-switch-label">
         Don't have an account? Sign up
