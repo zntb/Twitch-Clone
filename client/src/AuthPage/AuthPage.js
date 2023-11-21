69bb293d50c0e81ce36e568283bdac85f09a9e82
zntb <zenetibi@yahoo.com>
2023-11-21 14:59:14 +0200

    Add Login.js, Register.js, components and add toggle logic

--- client/src/AuthPage/AuthPage.js
+++ client/src/AuthPage/AuthPage.js
@@ -1,5 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Login } from './Login';
+import { Register } from './Register';
 
 export const AuthPage = () => {
-  return <div>Hello here is auth page!</div>;
+  const [isLogin, setIsLogin] = useState(true);
+
+  const handleAuthPageToggle = () => {
+    setIsLogin((prev) => !prev);
+  };
+  return (
+    <div>
+      {isLogin ? (
+        <Login switchAuthHandler={handleAuthPageToggle} />
+      ) : (
+        <Register switchAuthHandler={handleAuthPageToggle} />
+      )}
+    </div>
+  );
 };
