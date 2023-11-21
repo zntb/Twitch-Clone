e7aaa2b0ee24ac8676056923406a0e4763908b7e
zntb <zenetibi@yahoo.com>
2023-11-21 09:06:55 +0200

    Introdution to JWT

--- server/src/controllers/auth/postRegister.js
+++ server/src/controllers/auth/postRegister.js
@@ -1,11 +1,18 @@
 import User from '../../models/User.js';
 
 export const postRegister = async (req, res) => {
-  const user = await User.create({
-    username: 'John',
-    email: 'test@test.com',
-    password: '123456',
-  });
+  try {
+    const { username, email, password } = req.body;
+
+    const userExists = await User.exists({ email });
+
+    if (userExists) {
+      return res.status(409).send('E-mail already in use!');
+    }
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send('Error occured, please try again.');
+  }
 
   return res.send('user has been added to database');
 };
