c55374e77fdc4b92bf9b228b1070583fe6a8921b
zntb <zenetibi@yahoo.com>
2023-11-21 16:35:34 +0200

    Create reusable input component

---
+++ client/src/AuthPage/AuthInput.js
@@ -0,0 +1,37 @@
+import React from 'react';
+
+export const AuthInput = ({
+  field,
+  label,
+  value,
+  onChangeHandler,
+  type,
+  showErrorMessage,
+  validationMessage,
+  onBlurHandler,
+}) => {
+  const handleValueChange = (event) => {
+    onChangeHandler(event.target.value, field);
+  };
+
+  const handleInputBlur = (event) => {
+    onBlurHandler(event.target.value, field);
+  };
+
+  return (
+    <>
+      <div className="auth-form-label">
+        <span>{label}</span>
+      </div>
+      <input
+        type={type}
+        value={value}
+        onChange={handleValueChange}
+        onBlur={handleInputBlur}
+      />
+      <span className="auth-form-validation-message">
+        {showErrorMessage && validationMessage}
+      </span>
+    </>
+  );
+};
--- client/src/AuthPage/Login.js
+++ client/src/AuthPage/Login.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import { Logo } from './Logo';
+import { AuthInput } from './AuthInput';
 
 export const Login = ({ switchAuthHandler }) => {
   return (
     <div className="login-container">
       <Logo text={'Log in to Clone'} />
-      <form className="auth-form">Form</form>
+      <form className="auth-form">
+        <AuthInput field={'email'} label={'Email'} />
+        <AuthInput field={'password'} label={'Password'} />
+        <button>Log In</button>
+      </form>
       <span onClick={switchAuthHandler} className="auth-form-switch-label">
         Don't have an account? Sign up
       </span>
