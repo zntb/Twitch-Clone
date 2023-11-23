cad8ddd506c8cb0633aec0c8b76bc0541781ec8b
zntb <zenetibi@yahoo.com>
2023-11-23 15:41:37 +0200

    Fetching Real Channel Data from DB

--- server/src/controllers/channels/getChannelDetails.js
+++ server/src/controllers/channels/getChannelDetails.js
@@ -1,10 +1,32 @@
+import User from '../../models/User.js';
+import Channel from '../../models/Channel.js';
+
 export const getChannelDetails = async (req, res) => {
-  return res.status(200).json({
-    id: 1,
-    title: 'Channel',
-    description: 'Dummy decription',
-    username: 'Tibi',
-    isOnline: false,
-    streamUrl: 'http',
-  });
+  try {
+    const { channelId } = req.params;
+
+    const channel = await Channel.findById(channelId);
+
+    if (!channel || !channel.isActive) {
+      return res.status(404).send('Channel not found!');
+    }
+
+    const user = await User.findOne({ channel: channelId }, { username: 1 });
+
+    const streamUrl = 'http';
+
+    const isOnline = false;
+
+    return res.status(200).json({
+      id: channel._id,
+      title: channel.title,
+      description: channel.description,
+      username: user.username,
+      isOnline,
+      streamUrl: streamUrl,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send('Something went wrong!');
+  }
 };
