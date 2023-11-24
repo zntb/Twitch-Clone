73ca2cbc6e58cb823a626ccd8b4c848d718bc87f
zntb <zenetibi@yahoo.com>
2023-11-24 14:35:39 +0200

    Update GetChannelSettings.js Fetching Settings from DB

--- server/src/controllers/settings/getChannelSettings.js
+++ server/src/controllers/settings/getChannelSettings.js
@@ -4,10 +4,23 @@ export const getChannelSettings = async (req, res) => {
   try {
     const { userId } = req.user;
 
-    console.log(userId);
+    const userData = await User.findById(userId, {
+      channel: 1,
+      username: 1,
+    }).populate('channel');
 
-    return res.status(200).send('this route is secured');
+    console.log(userData);
+
+    return res.status(200).json({
+      id: userData.channel._id,
+      username: userData.username,
+      title: userData.channel.title,
+      description: userData.channel.description,
+      avatarUrl: userData.channel.avatarUrl,
+      streamKey: userData.channel.streamKey,
+    });
   } catch (error) {
+    console.log(error);
     return res.status(500).send('Something went wrong!');
   }
 };
