896a93c32d3bf77f37b573b8b7b0d21f78821654
zntb <zenetibi@yahoo.com>
2023-12-02 17:26:35 +0200

    Preparing to render real Channels

--- client/src/DashboardPage/DashboardPage.js
+++ client/src/DashboardPage/DashboardPage.js
@@ -3,22 +3,28 @@ import { Nav } from './Nav';
 import { Sidebar } from './Sidebar';
 import { Content } from './Content';
 import { useChannels, useUserDetails } from '../shared/hooks';
+import { LoadingSpinner } from '../shared/components';
 
 import './dashboardPage.css';
 
 export const DashboardPage = () => {
-  const { getChannels } = useChannels();
+  const { getChannels, isFetching, followedChannels, allChannels } =
+    useChannels();
   const { isLogged } = useUserDetails();
 
   useEffect(() => {
     getChannels(isLogged);
   }, []);
 
+  if (isFetching) {
+    return <LoadingSpinner />;
+  }
+
   return (
     <div className="dashboard-container">
       <Nav />
       <Sidebar />
-      <Content />
+      <Content channels={allChannels} />
     </div>
   );
 };
