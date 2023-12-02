2aa10b950e1dd7e71d765cf294d91e5f1204ac14
zntb <zenetibi@yahoo.com>
2023-12-02 17:04:19 +0200

    Fetching Channels Hook

---
+++ client/src/shared/hooks/useChannels.js
@@ -0,0 +1,49 @@
+import { useEffect, useState } from 'react';
+import { toast } from 'react-hot-toast';
+import {
+  getFollowedChannels,
+  getChannels as getChannelsRequest,
+} from '../../api';
+
+export const useChannels = () => {
+  const [channels, setChannels] = useState(null);
+
+  const getChannels = async (isLogged = false) => {
+    const channelsData = await getChannelsRequest();
+
+    if (channelsData.error) {
+      return toast.error(
+        channelsData.exception?.response?.data ||
+          'Error occured when fetching the channels!'
+      );
+    }
+
+    if (!isLogged) {
+      return setChannels({
+        channels: channelsData.data.channels,
+      });
+    }
+
+    const followedChannelsData = await getFollowedChannels();
+
+    if (followedChannelsData.error) {
+      return toast.error(
+        followedChannelsData.exception?.response?.data ||
+          'Error occured when fetching the followed channels!'
+      );
+    }
+
+    setChannels({
+      channels: channelsData.data.channels,
+      followedChannels: followedChannelsData.data.followedChannels,
+    });
+  };
+
+  useEffect(() => {
+    console.log(channels);
+  }, [channels]);
+
+  return {
+    getChannels,
+  };
+};
