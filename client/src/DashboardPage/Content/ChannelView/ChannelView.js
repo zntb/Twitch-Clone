421907892e25d44a3417d32df77c4c65c1325284
zntb <zenetibi@yahoo.com>
2023-12-04 15:44:20 +0200

    Displaying Stream From Media Server

--- client/src/DashboardPage/Content/ChannelView/ChannelView.js
+++ client/src/DashboardPage/Content/ChannelView/ChannelView.js
@@ -1,9 +1,22 @@
 import React, { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
+import { ReactFlvPlayer } from 'react-flv-player';
 import { Chat } from '../ChannelView/Chat/Chat.js';
 import { ChannelDescription } from './ChannelDescription.js';
 import { useChannelDetails } from '../../../shared/hooks/useChannelDetails.js';
 import { LoadingSpinner } from '../../../shared/components/LoadingSpinner.js';
+
+export const Stream = () => {
+  return (
+    <div className="channel-video-container">
+      <ReactFlvPlayer
+        width="100%"
+        height="100%"
+        url="http://localhost:8000/live/e6f1b864-2353-4fd8-af94-bea1583f4495.flv"
+      />
+    </div>
+  );
+};
 
 export const ChannelView = ({ getChannels }) => {
   const { isFetching, getChannelDetails, channelDetails } = useChannelDetails();
@@ -21,9 +34,10 @@ export const ChannelView = ({ getChannels }) => {
   return (
     <div className="channel-container">
       <div className="channel-video-description-section">
-        <div className="channel-offline-placeholder">
+        {/* <div className="channel-offline-placeholder">
           <span>Channel is offline</span>
-        </div>
+        </div> */}
+        <Stream />
         <ChannelDescription
           channelId={channelDetails.id}
           title={channelDetails.title}
